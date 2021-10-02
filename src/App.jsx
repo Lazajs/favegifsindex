fb5e6907cb4aa33effad26f66c311e8b307cf8df
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2021-10-01 21:14:33 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
   }
   render() { 
     return ( 
-      <Stilished/>
+      <Stilished color="#000a"/>
     );
   }
 }
--- src/Components/StyledComp.jsx
+++ src/Components/StyledComp.jsx
@@ -1,19 +1,56 @@
 import React, {Component} from 'react'
+import { Fragment } from 'react/cjs/react.production.min';
 import styled from 'styled-components'
+
+
+class LoginBut extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <>
+                <button onClick={this.props.onClick}>Login</button>
+                <p>Login to continue</p>
+            </>
+         );
+    }
+}
+ 
+class LogoutBut extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <>
+                <button onClick={this.props.onClick}>Logout</button>
+                <p>Do you want to logout?</p>
+            </>
+         );
+    }
+}
+
+
+
+
 
 class Stilished extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = { isLogged: false }
     }
+
+    handleLog() {
+        this.setState(prevState => ({isLogged: !prevState.isLogged}))
+    } 
+
     render() {
-        const Title = styled.h1 `
-        color: red;
-        font-size: 50px;
-        `
-        return (
-            <Title>Hello</Title>
-        );
+       return this.state.isLogged
+        ?    <LogoutBut onClick={()=> this.handleLog()} />    
+        :    <LoginBut onClick={()=> this.handleLog()} />
     }
 }
 
