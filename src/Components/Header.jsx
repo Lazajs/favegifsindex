1004756df07d53fadf9382e194e8835938c9a571
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2021-10-20 18:18:51 -0300

    Add responsive styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,14 @@ const Header = ({topic, rating, form})=>{
     return ( 
         <>
             <header className="header shadows">
-                <h1 className="header__heading">faveGifs</h1>
-                <form ref={form} className="header__form" action="get">
-                    <input ref={topic} type="text" placeholder="Enter a topic"/>
-                    <select alt="rating" ref={rating}> //.selectedOptions() array .selectedIndex()
+                <div className="header__text">
+                    <h1 className="header__heading">faveGifs</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare tempus dapibus. Aliquam imperdiet enim sit amet mauris pretium, nec porta arcu egestas. Sed tellus turpis, aliquam et urna congue, ullamcorper scelerisque eros.</p>
+                </div>
+            </header>
+            <form ref={form} className="header__form" action="get">
+                <input ref={topic} type="text" placeholder="Start finding your favourite gifs by typing a topic!"/>
+                    <select alt="rating" ref={rating}>
                             <option value="g">g</option>
                             <option value="pg">pg</option>
                             <option value="pg-13">pg-13</option>
@@ -16,7 +20,6 @@ const Header = ({topic, rating, form})=>{
                     </select>
                     <input type="submit" className="search" value=" "/>
                 </form>
-            </header>
         </>
     )
 }
