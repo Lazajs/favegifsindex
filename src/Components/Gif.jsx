6ac8e3a8be28d3e2e9fba4620235341b39b83b96
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2021-11-12 00:59:05 -0300

    Add column system

--- src/Components/Gif.jsx
+++ src/Components/Gif.jsx
@@ -4,7 +4,7 @@ import './Gif.scss'
 const Gif = ({url, title})=>{
     return  <div className="gif-container">     
                 <img onError={()=> <h2> Sorry, there was a problem loading the gif :(</h2>} alt={title} className="gif-img" src={url} />
-                <p>{title}</p>
+                <p>{title.substring(0,title.indexOf('GIF'))}</p>
             </div>
 }
 
